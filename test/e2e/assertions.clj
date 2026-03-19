748c74362e99f0849ac44f4a85d61de5ed2f48f9
Piotr Kaznowski <piotr@kazno.dev>
2026-03-19 23:28:14 +0100

    [test] add home-layout assertion to reduce e2e path boilerplate

--- test/e2e/assertions.clj
+++ test/e2e/assertions.clj
@@ -37,3 +37,41 @@
       (string? value) (str/includes? value expected)
       (sequential? value) (boolean (some #{expected} value))
       :else false)))
+
+(defn- home-path [home rel-path]
+  (str home rel-path))
+
+(defn- symlink-target? [path expected-target]
+  (and (fs/sym-link? path)
+       (= expected-target (str (fs/read-link path)))))
+
+(defn- regular-file? [path]
+  (and (fs/exists? path)
+       (fs/regular-file? path)
+       (not (fs/sym-link? path))))
+
+(defn- json-path-contains? [path ks expected]
+  (let [value (get-in (read-json path) ks)]
+    (cond
+      (string? value) (str/includes? value expected)
+      (sequential? value) (boolean (some #{expected} value))
+      :else false)))
+
+(defmethod assertions/check-expectation :home-layout
+  [[_ home {:keys [symlinks regular-files missing json-equals json-contains]}] _context]
+  (and
+   (every? (fn [[rel-path expected-target]]
+             (symlink-target? (home-path home rel-path) expected-target))
+           (or symlinks []))
+   (every? (fn [rel-path]
+             (regular-file? (home-path home rel-path)))
+           (or regular-files []))
+   (every? (fn [rel-path]
+             (not (fs/exists? (home-path home rel-path))))
+           (or missing []))
+   (every? (fn [[rel-path ks expected]]
+             (= expected (get-in (read-json (home-path home rel-path)) ks)))
+           (or json-equals []))
+   (every? (fn [[rel-path ks expected]]
+             (json-path-contains? (home-path home rel-path) ks expected))
+           (or json-contains []))))
